fe0d2a1e404d66994f74983e7f77b0b9911b1d9c
DemonsIT <1306205691@qq.com>
2019-11-23 22:27:11 +0800

    python学习

---
+++ base01/demo04.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# _*_ coding:UTF-8 _*_
+
+"""
+_title_ = ''
+_author_ = '♞Demons♛'
+_date_ = '2019-08-11'
+"""
+a = 10
+print(a)
---
+++ com/google/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# _*_ coding:UTF-8 _*_
+
+"""
+_title_ = ''
+_author_ = '♞Demons♛'
+_date_ = '2019-11-22'
+"""
