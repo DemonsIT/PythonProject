42a9faf5d25b99436876e90156dba7d385740974
DemonsIT <1306205691@qq.com>
2020-01-10 22:13:24 +0800

    python学习

---
+++ com/google/test01/TestPrint.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# _*_ coding:UTF-8 _*_
+
+"""
+_title_ = ''
+_author_ = '♞Demons♛'
+_date_ = '2020-01-10'
+"""
+print('first python print')
+# 括号中有多个参数时默认使用空格分隔
+print('first', 'second', 'third')
+# 支持多种类型参数的拼接
+print('string', 2, 8.6)
