8c31530b5be4e5a2e3a053f67d480eae259877d3
DemonsIT <1306205691@qq.com>
2020-01-12 15:44:02 +0800

    调用函数

---
+++ com/google/function/FunctionTest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# _*_ coding:UTF-8 _*_
+
+"""
+_title_ = ''
+_author_ = '♞Demons♛'
+_date_ = '2020-01-12'
+"""
+# python内置了很多函数,可以直接调用
+print(abs(100))
+print(abs(-100))
+print(abs(10.45))
+# 求多个数的最大值
+print(max(1, 2, 3, 4, 5))
+# 数据类型转换函数
+int('123')
+int(1.234)
+float('12.34')
+str(1.234)
+bool(1)
+bool('')
+
---
+++ com/google/function/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# _*_ coding:UTF-8 _*_
+
+"""
+_title_ = ''
+_author_ = '♞Demons♛'
+_date_ = '2020-01-12'
+"""
